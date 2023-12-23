3b2db2e984d2dd33fa79493d1ea7717d773c8ef4
Oleg <oleg.pak1996@gmail.com>
2023-12-23 11:32:17 +0300

    +

---
+++ lib/pages/home/counter_panel.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CounterPanel extends StatelessWidget {
+  const CounterPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white, borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          GestureDetector(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
