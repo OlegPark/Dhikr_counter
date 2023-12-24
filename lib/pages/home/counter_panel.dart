912d432d6d083893fa80aafaeefdce6d60d90ef9
Oleg <oleg.pak1996@gmail.com>
2023-12-24 22:45:17 +0300

    Добавил Counter panel

--- lib/pages/home/counter_panel.dart
+++ lib/pages/home/counter_panel.dart
@@ -1,3 +1,4 @@
+import 'package:dhikr_counter/constanst/constants.dart';
 import 'package:flutter/material.dart';
 
 class CounterPanel extends StatelessWidget {
@@ -5,25 +6,94 @@ class CounterPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          GestureDetector(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.blue,
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 24),
+          margin: const EdgeInsets.only(bottom: 14),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: alphaBlueLight,
+                  ),
+                  height: 35,
+                  width: 35,
+                ),
               ),
-              height: 35,
-              width: 35,
+              GestureDetector(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: alphaBlue,
+                  ),
+                  height: 154,
+                  width: 154,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(height: 15),
+                      Text(
+                        '33',
+                        style: TextStyle(
+                          fontSize: 48,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                      ),
+                      Text(
+                        'Dhikr',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                          height: 1,
+                        ),
+                      ),
+                      SizedBox(height: 1),
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: alphaBlueLight,
+                  ),
+                  height: 35,
+                  width: 35,
+                ),
+              ),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.white,
+            ),
+            height: 45,
+            alignment: Alignment.center,
+            margin: const EdgeInsets.only(bottom: 14),
+            child: const Text(
+              'Save Dhikr',
+              style: TextStyle(
+                color: alphaBlue
+              ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
