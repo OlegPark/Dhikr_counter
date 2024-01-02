3ecf6e4a817ddc4b7415ac7fe50458ee49ef7def
Oleg <oleg.pak1996@gmail.com>
2024-01-02 22:37:06 +0300

    +

--- lib/pages/home/db_panel.dart
+++ lib/pages/home/db_panel.dart
@@ -42,7 +42,19 @@ class DBPanel extends StatelessWidget {
                       borderRadius: BorderRadius.circular(10),
                     ),
                     margin: const EdgeInsets.only(top: 10),
-                    child: Center(child: Text('$index'),),
+                    child: Row(
+                      children: [
+                        Text('$index'),
+                        Expanded(
+                          child: Text(
+                            'Заголовок который показывается в Зикре в нашем приложении'),
+                        ),
+                        Text('19.02.2023'),
+                        GestureDetector(
+                          child: Icon(Icons.do_not_step),
+                        ),
+                      ],
+                    ),
                   );
                 },
               ),
