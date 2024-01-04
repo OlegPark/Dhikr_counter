c1ad4c1e2396c3f7363022d9f6507e45c5bd48cb
Oleg <oleg.pak1996@gmail.com>
2024-01-04 14:07:58 +0300

    +

--- lib/pages/home/db_panel.dart
+++ lib/pages/home/db_panel.dart
@@ -41,19 +41,25 @@ class DBPanel extends StatelessWidget {
                       borderRadius: BorderRadius.circular(10),
                     ),
                     margin: const EdgeInsets.only(top: 10),
+                    padding: const EdgeInsets.only(left: 5),
                     child: Row(
                       children: [
-                        Text(
-                          '$index',
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: alphaBlue,
+                        SizedBox(
+                          width: 50,
+                          child: Text(
+                            index == 0 ? '234' : '$index',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: alphaBlue,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
                         ),
                         Container(
                           height: 30,
                           width: 1,
+                          margin: const EdgeInsets.only(right: 10, left: 5),
                           color: Colors.white,
                         ),
                         const Expanded(
@@ -66,6 +72,7 @@ class DBPanel extends StatelessWidget {
                             maxLines: 1,
                           ),
                         ),
+                        const SizedBox(width: 15,),
                         const Text(
                           '19.02.2023',
                           style: TextStyle(
@@ -74,14 +81,12 @@ class DBPanel extends StatelessWidget {
                           ),
                         ),
                         GestureDetector(
-                          onTap: () {
-                            print("object");
-                          },
+                          onTap: () {},
                           child: Container(
                             color: Colors.transparent,
                             height: 48,
                             width: 48,
-                            child: Icon(Icons.do_not_step),
+                            child: const Icon(Icons.do_not_step),
                           ),
                         ),
                       ],
