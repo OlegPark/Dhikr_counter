522415854d8416c07d821068866928a4ff266f1f
Oleg <oleg.pak1996@gmail.com>
2024-01-31 00:03:08 +0300

    Добавил go_router

---
+++ lib/pages/settings/settings.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Settings'
+          ),
+      ),
+      body: const Center(
+        child: Text('settings'),
+      ),
+    );
+  }
+}
